97ac3e14a13eb2f364a983fbdf846589e240c0b5
d.matveyev <d.matveyev@dssl.ru>
2025-06-07 09:29:19 +0300

    mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,19 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+const getHole = index => document.getElementById(`hole${index}`);
+
+for (let i = 1; i <= 9; i++) {
+    const hole = getHole(i);
+    hole.onclick = () => {
+        if (hole.className.includes('hole_has-mole')) {
+            dead.textContent++;
+        } else {
+            lost.textContent++;
+        }
+        if (dead.textContent === "10") {
+            alert('Победа!');
+        } else if (lost.textContent === "5") {
+            alert('Поражение!');
+        }
+    };
+}
